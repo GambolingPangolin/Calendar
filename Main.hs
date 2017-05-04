28681cebf30f4127243b648c62322af3a67f9b7d
Ian Shipman <ics.git@gambolingpangolin.com>
2017-05-04 14:56:15 -0400

    Parser coded.  Need to fix a bug where integers less than 10 show up without the leading zero.

--- Main.hs
+++ Main.hs
@@ -11,3 +11,86 @@
 -- Pseudocode for the parser
 -- header >> loop parseInterval indent=0 | parsePoint indent=0
 -- parsePoint n = (n tab) >> dateTime >> body >> ?note >> repeat >> newLine >> loop parsePoint (n+1) 
+
+import Data.List (partition)
+
+import Text.Parsec            as P
+import Text.Parsec.ByteString (Parser, parseFromFile)
+import Text.Parsec.Char       as PC
+
+import Structure (Entry(..), Date(..), Time(..), Repeat(..))
+
+inputFile = "/home/ian/Dropbox/calendar.text"
+ouputFile = "/home/ian/Code/Haskell/Calendar/output.text"
+pastFile  = "/home/ian/Code/Haskell/Calendar/past.text"
+
+main :: IO ()
+main = 
+  do
+    res <- parseFromFile 
+             (try (string "CALENDAR" >> newline) >> many (entry 0))
+             inputFile
+    case res of 
+      Left err ->
+        print err
+      Right cal ->
+        let
+          -- Take out the repeating items
+          -- (reps, rem) = partition isRepeating cal
+          -- (past, cur) = partition (isPast today) rem 
+          -- newCal      = update reps cur
+        in
+        --writeFile outputFile $ show newCal >>
+        --writeFile pastFile $ show pastCal
+        print cal
+  
+-- PARSERS
+date :: Parser Date
+date =
+  let
+    r = read :: String -> Int
+  in
+  Date
+    <$> (r <$> many digit)
+    <*> (r <$> (char '-' >> many digit))
+    <*> (r <$> (char '-' >> many digit))
+
+time :: Parser Time
+time =
+  let
+    r = read :: String -> Int
+  in
+  Time
+    <$> (r <$> many digit)
+    <*> (r <$> (char ':' >> many digit))
+
+repetition :: Parser Repeat
+repetition =
+  let
+    daily   = char 'd' >> (return Daily)
+    weekly  = char 'w' >> (return Weekly)
+    monthly = char 'm' >> (return Monthly)
+    yearly  = char 'y' >> (return Yearly)
+  in
+  char '^' >> (daily <|> weekly <|> monthly) 
+
+parseNote :: Parser String
+parseNote =
+  between (char '(') (char ')') (many $ noneOf "\n)")
+
+entry :: Int -> Parser Entry
+entry n =
+  let
+    tryMaybe = optionMaybe . try
+    subEntries = newline >> many (entry (n+1))
+  in
+  count n (char '\t')
+  >> Entry
+    <$> date
+    <*> tryMaybe (spaces >> time)
+    <*> tryMaybe (spaces >> date)
+    <*> tryMaybe (spaces >> time)
+    <*> (spaces >> many (noneOf "\n^("))
+    <*> tryMaybe (spaces >> parseNote)
+    <*> tryMaybe (spaces >> repetition)
+    <*> (try subEntries <|> return [])
--- Structure.hs
+++ Structure.hs
@@ -1,21 +1,135 @@
-{-
-	A calendar item has the structure
-	STARTDATE [STARTTIME] [ENDDATE [ENDTIME]] BODY [(NOTE)] [REPEAT]
--}
-data CalItem = Point {
-    date :: DateTime,
-    body :: CalBody,
-    repeat :: Repeat,
-    subItems :: [CalItem]}
-  | Interval {
-    start :: DateTime,
-    end :: DateTime,
-    body :: CalBody,
-    repeat :: Repeat,
-    subItems :: [CalItem]}
+{-|	Structures for the calendar parsing app
 
-data CalBody = Body | (Body, Note)
-data DateTime = Date | (Date, Time)
-data Body = String
-data Note = String
-data Repeat = Daily | Weekly | Monthly | None
+-}
+
+module Structure (
+  Entry(..)
+  , Date(..)
+  , Time(..)
+  , Repeat(..)
+  , repeating
+  ) where
+
+import Control.Monad (liftM)
+import Data.Function ((&))
+import Data.List (intercalate, concat)
+import Data.Maybe (isJust, maybe)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.Time.Calendar (toGregorian)
+
+-- Preferred syntax for reverse function application
+(|>) = (&)
+
+{-| A calendar entry has the form
+ -  STARTDATE [STARTTIME] [ENDDATE] [ENDTIME] BODY [(NOTE)] [REPEAT]
+
+-}
+data Entry = Entry
+  { startDate :: Date
+  , startTime :: Maybe Time
+  , endDate :: Maybe Date
+  , endTime :: Maybe Time
+  , body :: String
+  , note :: Maybe String
+  , rep :: Maybe Repeat
+  , subEntries :: [Entry]
+  } deriving Eq
+
+instance Show Entry where
+  show e =
+    let
+
+      mss :: Show a => Maybe a -> [String]
+      mss = maybe [] (return . show) 
+      
+      -- Format the sub-entries
+      se :: String
+      se = if length (subEntries e) == 0 
+             then ""
+             else 
+               '\t' : intercalate "\t" (map show $ subEntries e)
+
+    in
+    [ [show (startDate e)]
+    , mss (startTime e)
+    , mss (endDate e)
+    , mss (endTime e)
+    , [body e]
+    , maybe [] (\ s -> ['(' : (s ++ ")")]) $ note e
+    , mss (rep e)
+    ]
+    |> concat 
+    |> intercalate " "
+    |> flip (++) "\n"
+    |> flip (++) se 
+
+{-| We store dates as a record containing the month, day, and year.
+ 
+ -}
+data Date = Date
+  { month :: Int
+  , day :: Int
+  , year :: Int 
+  } deriving Eq
+
+instance Ord Date where
+  d1 <= d2 = 
+    year d1 < year d2
+    || (year d1 == year d2 && month d1 < month d2)
+    || (year d1 == year d2 && month d1 == month d2 && day d1 <= day d2)
+
+instance Show Date where
+  show d =
+    intercalate "-" (map show [month d, day d, year d])
+
+{-| Get the current date, formatted as a Date
+
+ -}
+today :: IO Date
+today =
+  let
+    fromTuple (y,m,d) = Date m d (fromInteger y)
+  in
+  liftM (fromTuple . toGregorian . utctDay) getCurrentTime
+
+{-| We represent times as records containing an hour and a minute where the hour is 1 through 24.
+
+-}
+data Time = Time
+  { hour :: Int
+  , minute :: Int
+  } deriving Eq
+
+instance Ord Time where
+  t1 <= t2 =
+    hour t1 < hour t2
+    || (hour t1 == hour t2 && minute t1 <= minute t2)
+
+instance Show Time where
+  show t =
+    show (hour t) ++ ":" ++ (show $ minute t) 
+
+{-| An event can repeat daily, weekly or monthly.
+ -  TODO: Add a constructor for more complex repetition.
+
+-}
+data Repeat = 
+  Daily 
+  | Weekly 
+  | Monthly 
+  | Yearly
+  deriving Eq
+
+instance Show Repeat where
+  show r =
+    case r of 
+      Daily   -> "^d"
+      Weekly  -> "^w"
+      Monthly -> "^m"
+      Yearly  -> "^y"
+
+{-| Check if an entry has a set repeat.
+
+-}
+repeating :: Entry -> Bool
+repeating = isJust . rep
